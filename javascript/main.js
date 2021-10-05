dabc37bc65794a43c4aa07f0a8cf39e2d9eeadbd
olispanda <91331850+olispanda@users.noreply.github.com>
2021-10-05 17:31:14 +0200

    feat: add scroll up animation, style basic slide

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,26 @@
 var elem = document.querySelector('.carousel');
 
+var scrollUpButtons = document.querySelectorAll('.button-see-more');
+
+// Add eventlistener for all buttons
+// scrollUpButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         console.log('Hoi Oli');
+//     })
+// })
+
+scrollUpButtons.forEach(button => {
+    button.addEventListener('click', seeMore)
+})
+
+// select all back to engine buttons, compare: see more buttons
+
+// add eventlistener for all back to engine buttons to listen for clicks
+// pass seeLess function to remove is ´is-top´ class
+
+
+console.log('scrollUpButtons: ', scrollUpButtons);
+
 var flickityOptionen = {
     cellAlign: 'left',
     contain: true,
@@ -12,12 +33,13 @@ var flkty = new Flickity(elem, flickityOptionen);
 function seeMore() {
     console.log('hallo')
     var container = document.querySelector('.container')
-    container.classList.add("scroll-up")
+    container.classList.add("is-top")
 }
+
 function seeLess() {
     console.log('tschau')
     var container = document.querySelector('.container')
-    container.classList.remove("scroll-up")
+    container.classList.remove("is-top")
 }
 
 
