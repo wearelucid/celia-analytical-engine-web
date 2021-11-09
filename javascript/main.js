c22049ab333dad475c881fc66007a50f15800db6
Jones <git@jonasscheiwiller.ch>
2021-11-09 15:49:16 +0100

    feat: stop video on back to engine button click

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,12 @@
 var elem = document.querySelector('.carousel')
 
 var flickityOptions = {
-    cellAlign: 'left',
-    contain: true,
-    wrapAround: true,
-    fullscreen: true,
-    pageDots: false,
-    draggable: true,
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  fullscreen: true,
+  pageDots: false,
+  draggable: true,
 }
 
 var flkty = new Flickity(elem, flickityOptions)
@@ -15,56 +15,56 @@ var scrollUpButtons = document.querySelectorAll('[data-scroll-up]')
 var scrollDownButtons = document.querySelectorAll('[data-scroll-down]')
 var container = document.querySelector('.container')
 
-console.log('scrollUpButtons: ', scrollUpButtons)
-console.log('scrollDownButtons: ', scrollDownButtons)
-
-scrollUpButtons.forEach(button => {
-    button.addEventListener('click', seeMore)
+scrollUpButtons.forEach((button) => {
+  button.addEventListener('click', seeMore)
 })
 
-scrollDownButtons.forEach(button => {
-    button.addEventListener('click', seeLess)
+scrollDownButtons.forEach((button) => {
+  button.addEventListener('click', seeLess)
 })
 
 function seeMore() {
-    scrollUp()
-    disableSwipe()
+  scrollUp()
+  disableSwipe()
 }
 
-function seeLess() {
-    scrollDown()
-    enableSwipe()
+function seeLess(event) {
+  scrollDown()
+  enableSwipe()
+  stopNearestVideo(event.target)
 }
 
 function scrollDown() {
-    container.classList.remove('is-top')
+  container.classList.remove('is-top')
 }
 
 function scrollUp() {
-    container.classList.add('is-top')
+  container.classList.add('is-top')
 }
 
 function disableSwipe() {
-    flkty.options.draggable = false
-    flkty.updateDraggable()
+  flkty.options.draggable = false
+  flkty.updateDraggable()
 }
 
 function enableSwipe() {
-    flkty.options.draggable = true
-    flkty.updateDraggable()
+  flkty.options.draggable = true
+  flkty.updateDraggable()
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Stopping Vimeo Video when returning to slide content
+// https://gist.github.com/cferdinandi/9044694
+function stopNearestVideo(element) {
+  const videoDiv = element.closest('.video')
+  if (videoDiv) {
+    const iframe = videoDiv.querySelector('iframe')
+    const video = iframe.querySelector('video')
+    if (iframe) {
+      const iframeSrc = iframe.src
+      iframe.src = iframeSrc
+    }
+    if (video) {
+      video.pause()
+    }
+  }
+}
